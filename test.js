afbfef95e529924e7116d9bc6d4e95a5d7b49686
Frederick-S <mao_xiaodan@hotmail.com>
2014-10-15 07:52:00 +0000

    Update test

--- test.js
+++ test.js
@@ -25,8 +25,8 @@ describe('Translate words', function () {
         assert.equal('123', arubedo.translate('123'));
     });
     
-    it('"Al Bhed語ソマ、FF10シソフギョフヌウアルフオデンゾベワウ。" should be translated to "Al Bhed語とは、FF10にとうじょうするかくうのげんごである。"', function() {
-        assert.equal('Al Bhed語とは、FF10にとうじょうするかくうのげんごである。', arubedo.translate('Al Bhed語とは、FF10シソフギョフヌウアルフオデンゾベワウ。'));
+    it('"Al Bhed 語ソマ、FF10シソフギョフヌウアルフオデンゾベワウ。" should be translated to "Al Bhed 語とは、FF10にとうじょうするかくうのげんごである。"', function() {
+        assert.equal('Al Bhed 語とは、FF10にとうじょうするかくうのげんごである。', arubedo.translate('Al Bhed 語とは、FF10シソフギョフヌウアルフオデンゾベワウ。'));
     });
 });
 
