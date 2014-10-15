2e796f92a85e3319bd69ca46db2f2f38c746c18f
Frederick-S <mao_xiaodan@hotmail.com>
2014-10-15 02:52:51 -0400

    Init travis ci

---
+++ test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var arubedo = require('./lib/arubedo.js');
+
+describe('Empty input', function () {
+	it('Should return empty string', function() {
+		assert.equal('', arubedo.translate());
+	});
+});
